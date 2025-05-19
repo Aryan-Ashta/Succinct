e7f52fc829da9cc642f080296ea24f86ef03a8b0
Aryan-Ashta <aashta221@gmail.com>
2025-05-19 09:28:41 -0400

    partially added drag to create event

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,31 @@
 import { CalendarBody, CalendarContainer, CalendarHeader } from '@howljs/calendar-kit';
 import React from 'react';
+
 //Should show events from plannin and should be able to add events manually
-const Calendar = () => {
+const MyCalendar = () => {
+  const handleDragCreateStart = (start: any) => {
+    console.log("Started creating event at:", start);
+    // You can use this to show a UI indicator that event creation has started
+  };
+
+  const handleDragCreateEnd = (event: any) => {
+    console.log("New event:", event);
+    // Here you would typically add the new event to your events array
+    // and possibly open a modal for the user to add more details
+    
+  };
+
   return (
-    <CalendarContainer numberOfDays={7}>
+    <CalendarContainer
+      allowDragToCreate={true}
+      onDragCreateEventStart={handleDragCreateStart}
+      onDragCreateEventEnd={handleDragCreateEnd}
+      // ... other props
+    >
       <CalendarHeader />
       <CalendarBody />
     </CalendarContainer>
   );
 };
 
-export default Calendar;
+export default MyCalendar;
