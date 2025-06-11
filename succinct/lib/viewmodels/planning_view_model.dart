7916dc802cf15c62e75b9a7cc8b981f19ca3b088
Aryan-Ashta <aashta221@gmail.com>
2025-06-11 12:03:37 -0400

    added elements to account pages

--- succinct/lib/viewmodels/planning_view_model.dart
+++ succinct/lib/viewmodels/planning_view_model.dart
@@ -1,3 +1,4 @@
 //call gemini here to generate the planning view model
-// store the chats in the model
+//each chat will be a different block 
+import 'package:succinct/services/planning_services.dart';
 
--- succinct/lib/views/account_views.dart
+++ succinct/lib/views/account_views.dart
@@ -19,106 +19,107 @@ class LoginRouteState extends State <LoginRoute> {
   String _password = '';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children:<Widget>[
-            Padding(
-              padding: EdgeInsets.only(left:15.0, right: 15.0, top: 15.0, bottom: 15.0),
-              child: Text(
-                'Log In',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:15.0, right: 15.0, top: 15.0, bottom: 15.0),
-              child:TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _email = value!;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: "example@gmail.com"
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:15.0, right: 15.0, top: 15.0, bottom: 15.0),
-              child:TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _password = value!;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                  hintText: "Enter a secure password",
-                ),
-                obscureText: true,
-              ),
-            ),
-            SizedBox(
-              height: 65,
-              width: 360,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green.shade900,
-                    foregroundColor: Colors.green,
-                  ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      signInWithEmail(_email, _password,context);
-                      context.go('/');
-                    }
-                  },
+      backgroundColor: Colors.green.shade100,
+      body: 
+          Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
                   child: Text(
-                    'Log in ',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    'Log In',
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.green.shade900),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 90,
-              width: 360,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green.shade900,
-                    foregroundColor: Colors.green,
-                  ),
-                  onPressed: () {
-                    context.go('/signup');
-                  },
-                  child: Text(
-                    "Don't have an account? Sign Up",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _email = value!;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: "example@gmail.com"
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      hintText: "Enter a secure password",
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 65,
+                  width: 360,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade900,
+                        foregroundColor: Colors.green,
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          signInWithEmail(_email, _password, context);
+                          context.go('/');
+                        }
+                      },
+                      child: Text(
+                        'Log in ',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 360,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade900,
+                        foregroundColor: Colors.green,
+                      ),
+                      onPressed: () {
+                        context.go('/signup');
+                      },
+                      child: Text(
+                        "Don't have an account? Sign Up",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
     );
   }
 }
@@ -140,6 +141,7 @@ class SignupRouteState extends State <SignupRoute> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.green.shade100,
       body: Form(
         key: _formKey,
         child: Column(
@@ -148,7 +150,7 @@ class SignupRouteState extends State <SignupRoute> {
               padding: EdgeInsets.only(left:15.0, right: 15.0, top: 15.0, bottom: 15.0),
               child:Text(
                 'Sign Up',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.green.shade900),
               ),
             ),
             Padding(
